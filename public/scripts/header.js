76f0c44a35efa58fcca252c7db37d33082b51688
hsunde <henrik.sunde@gmail.com>
2022-06-28 15:49:57 +0200

    Header and fonts

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -2,9 +2,9 @@ class HeaderUserControl extends React.Component {
     render() {
         if (this.props.user) {
             return [
-                <a key="upload" href="/upload">Upload</a>,
-                <a key="user" href={"/user/" + this.props.user}>{this.props.user}</a>,
-                <a key="logout" href="/logout">Log out</a>
+                <a key="upload" class="button" href="/upload">Upload</a>,
+                <a key="user" class="button" href={"/user/" + this.props.user}>{this.props.user}</a>,
+                <a key="logout" class="button" href="/logout">Log out</a>
             ]
         }
 
@@ -18,8 +18,8 @@ class Header extends React.Component {
     render() {
         return (
             <header>
-                <div id="logo">Simplepics</div>
-                <div id="usercontrol">
+                <div className="f24 semibold">Simplepics</div>
+                <div id="user_control" className="f18 semibold">
                     <HeaderUserControl user={this.props.user} />
                 </div>
             </header>
